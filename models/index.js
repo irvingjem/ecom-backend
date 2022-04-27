86aa9e74cc7b89ab0a022c7ff848be0805bca518
Your Name <irving.jem@gmail.com>
2022-04-26 17:11:32 -0700

    fixed models worked with tutor next step is to finish routes

--- models/index.js
+++ models/index.js
@@ -4,17 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+// Products belongsTo Category ex: One SHoe belongs to Sports  1: 1
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+// Categories have many Products ex: Sports Category has Shoes, rackets, shorts  1: M
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
+// Products belongToMany Tags (through ProductTag) M: M
+Product.belongsToMany(Tag, {
+        through: ProductTag,
+        foreignKey: 'product_id'
+    })
+    //Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+})
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
